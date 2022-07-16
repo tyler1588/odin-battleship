e4c8983c05c00118ad8571196018cde5a23d6c9d
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-15 22:42:49 -0400

    Feat: Add factory function to create ship
    ship properties include length, array of places hit, sunk true or false
    added tests for length, array of places hit, whether the ship is sunk or not

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './styles.css';
 
-(function helloWebpack(){
-    const container = document.getElementById('container');
-    const hello = document.createElement('h1');
-    hello.innerHTML = 'hello, webpack';
-    container.appendChild(hello);
-})();
+
+// (function helloWebpack(){
+//     const container = document.getElementById('container');
+//     const hello = document.createElement('h1');
+//     hello.innerHTML = 'hello, webpack';
+//     container.appendChild(hello);
+// })();
