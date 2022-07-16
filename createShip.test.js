f587aa72310840ec09b4db355d2cb4fc591fd801
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-16 00:27:26 -0400

    feat / refactor: Add function to attack ship / refactor isSunk function
    attack function takes a position as input, and marks that spot in the array as hit
    refactored isSunk function to be callable by the test

--- createShip.test.js
+++ createShip.test.js
@@ -3,6 +3,21 @@ import createShip from "./createShip";
 test('create ship with a length of 4', () => {
     const ship = createShip(4);
     expect(ship.length).toBe(4);
-    expect(ship.placesHit).toEqual(["Not Hit", "Not Hit", "Not Hit", "Not Hit"]);
-    expect(ship.sunk).toBe(false);
+})
+
+test('create ship 3 spots that are not hit', () => {
+    const ship = createShip(3);
+    expect(ship.placesHit).toEqual(["not hit", "not hit", "not hit"]);
+})
+
+test('check that ship is not sunk by default', () => {
+    const ship = createShip(2);
+    expect(ship.isSunk()).toBe(false);
+})
+
+test('check attack function', () => {
+    const ship = createShip(1);
+    expect(ship.attack(0)).toEqual();
+    expect(ship.placesHit).toEqual(["hit"]);
+    expect(ship.isSunk()).toBe(true);
 })
